e089be72368347e06180c18216bc62c3ee120160
Benjamin Gerber <ger.benjamin@gmail.com>
2013-02-22 16:10:12 +0100

    add footer and set index page

--- httpdocs/js/interfaces.js
+++ httpdocs/js/interfaces.js
@@ -321,7 +321,7 @@ function makeFooter () {
         height: 30,
         cls: 'footer-panel',
         html: '<div class="footer">'
-                + '<p>Wanderwerk version 1. Créé par benjamin gerber (ger.benjamin(at)gmail.com), supporté par les scouts Perceval de Moutier</p>'
+                + '<p>Wanderwerk version 1. Créé par <a href="http://ch.linkedin.com/in/benjamingerber" target="_blank" title="Voir le profil LinkedIn" >Benjamin Gerber</a>, supporté par les <a href="http://www.scout-perceval.ch/" target="_blank" title="Voir le site des Scouts Perceval de Moutier">Scouts Perceval de Moutier</a>. Application open source, disponible sur <a href="https://github.com/ger-benjamin/wanderwerk" target="_blank" title="Voir le code source">github</a>. Sous licence <a href="http://creativecommons.org/licenses/by-nc/2.5/ch/deed.fr" target="_blank" title="Voir la licence" >Creative Commons BY-NC</a></p>'
                 + '</div>'
     });
 }
